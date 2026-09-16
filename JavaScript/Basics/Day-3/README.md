bdcf8a1d38a53dc61ae09dec084f4f8286f28d02
abdullahazaad <abdullahazaad136@gmail.com>
2026-09-16 16:17:24 +0500

    day-3

---
+++ JavaScript/Basics/Day-3/README.md
@@ -0,0 +1,5 @@
+Topic 3 — Destructuring
+Destructuring ka simple matlab hai: object ya array ke andar se values ko easily nikal kar variables mein store karna.
+
+
+Destructuring JavaScript mein object ya array ke andar se values ko easily variables mein nikalne ka tareeqa hai. **Object destructuring** mein `{ }` use hota hai aur values property/name ke basis par milti hain, jaise `const { name, age } = user;`. **Array destructuring** mein `[ ]` use hota hai aur values position/order ke basis par milti hain, jaise `const [first, second] = fruits;`. React mein `useState()` ke saath array destructuring commonly use hoti hai, jaise `const [count, setCount] = useState(0);`.
---
+++ JavaScript/Basics/Day-3/index.js
@@ -0,0 +1,59 @@
+// 1. Object Destructuring
+// const user = {
+//     name: "abdullah",
+//     age: 21
+// };
+// const { name , age } = user;
+// console.log(name);
+// console.log(age);
+
+
+// 2. Array Destructuring
+// const numbers = [10, 20];
+// const [first, second] = numbers;
+// console.log(first);
+// console.log(second);
+
+
+
+// pratice-1
+// const user = {
+//     name: "Abdullah",
+//     age: 21,
+//     city: "Peshawar"
+// };
+// const { name , city } = user;
+// console.log(name);
+// console.log(city);
+
+
+
+// pratice-2
+// const product = {
+//     name: "Laptop",
+//     price: 8000,
+//     brand: "HP"
+// };
+// const { name , price } = product;
+// console.log(name);
+// console.log(price);
+
+
+
+// pratice-3
+// const fruits = ["Apple", "Banana", "Mango"];
+// const [ first , second ] = fruits;
+// console.log(first);
+// console.log(second);
+
+
+
+// pratice-4
+// const colors = ["Red", "Green", "Blue"];
+// const [ first , second, third ] = colors;
+// console.log(first);
+// console.log(second);
+// console.log(third);
+
+
+
