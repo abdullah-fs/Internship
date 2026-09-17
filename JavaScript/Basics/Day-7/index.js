a68e71ae8a75fd269d6aca38cf458d71119df013
abdullahazaad <abdullahazaad136@gmail.com>
2026-09-17 15:15:24 +0500

    Day-7

---
+++ JavaScript/Basics/Day-7/index.js
@@ -0,0 +1,27 @@
+// Closure
+// function outer() {
+//     let name = "Abdullah";
+
+//     function inner(){
+//         console.log(name);
+//     }
+
+//     return inner;
+// };
+// const result = outer();
+// result();
+
+
+// pratical use
+// function counter () {
+//     let count = 0;
+
+//     return function () {
+//         count ++;
+//         console.log(count);
+//     };
+// }
+// const increment = counter();
+// increment();
+// increment();
+// increment();
