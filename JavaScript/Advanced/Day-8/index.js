e5c6bd9ff501691cc5d61872a75bd98a43c58f1c
abdullahazaad <abdullahazaad136@gmail.com>
2026-09-20 02:46:21 +0500

    Day-8

---
+++ JavaScript/Advanced/Day-8/index.js
@@ -0,0 +1,57 @@
+// Callback Function
+// function greet(name) {
+//     console.log("Hello " + name);
+// }
+
+// function processUser(callback) {
+//     callback("Abdullah");
+// }
+
+// processUser(greet);
+
+
+// pratice-1
+// function add(a, b){
+//     return a + b;
+// }
+
+// function multiply(a, b){
+//     return a * b;
+// }
+
+// function calculate(a, b, callback){
+//     return callback(a, b);
+// }
+
+// console.log(calculate(10, 5, add));
+// console.log(calculate(10, 5, multiply));
+
+
+// pratice-2
+// function orderSuccess(product){
+//     console.log(product + "Order Successfully places");
+// }
+// function orderFailed(product){
+//     console.log(product + "Order Failed");
+// }
+// function processOrder(product, callback){
+//     callback(product);
+// }
+// processOrder("Laptop", orderSuccess);
+// processOrder("Mobile", orderFailed);
+
+
+// Higher order function
+// function greet(name){
+//     console.log("Hello" + name);
+// }
+// function processUser(callback){
+//     callback(" Abdullah");
+// }
+// processUser(greet);
+
+
+// onclick
+// function handleClick(){
+//     console.log("Button clicked");
+// }
