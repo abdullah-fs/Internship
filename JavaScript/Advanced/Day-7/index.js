da40aae53a422f642a69f6e1a26a460c4c8e04ac
abdullahazaad <abdullahazaad136@gmail.com>
2026-09-19 19:12:27 +0500

    Day-7

---
+++ JavaScript/Advanced/Day-7/index.js
@@ -0,0 +1,21 @@
+// Direct Mutation
+// const user = {
+//     name: "Abdullah",
+//     age: 21,
+//     city: "Peshawar"
+// };
+// user.age = 22;
+// console.log(user);
+
+
+// Immutable Way
+// const user = {
+//     name: "Abdullah",
+//     age: 22
+// };
+// const updateUser = {
+//     ...user,
+//     age: 23
+// };
+// console.log(user);
+// console.log(updateUser);
