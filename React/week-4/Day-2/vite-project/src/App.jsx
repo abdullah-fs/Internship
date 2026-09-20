57c5a40a34c9e80cf8c6a008744a2751c875739e
abdullahazaad <abdullahazaad136@gmail.com>
2026-09-20 18:43:59 +0500

    Day-2

---
+++ React/week-4/Day-2/vite-project/src/App.jsx
@@ -0,0 +1,103 @@
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <User name="Abdullah" age={21} />
+//   )
+// }
+
+// function User(props){
+//   return (
+//     <div>
+//       <h2>{props.name}</h2>
+//       <p>Age: {props.age}</p>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// pratice problem no 1
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <User
+//       name="Abdullah Mahmood"
+//       age={21}
+//       department="Software Enginnering"
+//       university="University of peshawar"
+//     />
+//   )
+// }
+
+// function User(props){
+//   return(
+//   <div>
+//     <h1>{props.name}</h1>
+//     <h2>{props.age}</h2>
+//     <h3>{props.department}</h3>
+//     <h3>{props.university}</h3>
+//   </div>
+//   )
+// }
+
+// export default App
+
+
+// pratice problem no 2
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <User
+//       name="Laptop"
+//       price={850000}
+//       brand="HP"
+//       category="Electronics"
+//     />
+//   )
+// }
+
+// function User(props){
+//   return(
+//     <div>
+//       <h1>{props.name}</h1>
+//       <h2>{props.price}</h2>
+//       <h3>{props.brand}</h3>
+//       <h3>{props.category}</h3>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// pratice problem no 3
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <User
+//       name="Abdullah"
+//       role="Frontend Developer"
+//       experience= "2 years"
+//       company="Microsoft"
+//     />
+//   )
+// }
+
+// function User(props){
+//   return(
+//     <div>
+//       <h1>{props.name}</h1>
+//       <h2>{props.role}</h2>
+//       <h3>{props.experience}</h3>
+//       <h3>{props.company}</h3>
+//     </div>
+//   )
+// }
+
+// export default App
+
