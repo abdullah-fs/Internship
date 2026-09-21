12ff8e743b29564400cbac5a9e701f7a28a212f9
abdullahazaad <abdullahazaad136@gmail.com>
2026-09-21 18:47:06 +0500

    Day-3

---
+++ React/week-4/Day-3/src/App.jsx
@@ -0,0 +1,111 @@
+// import React from 'react'
+// import { useState } from 'react'
+
+// const App = () => {
+//   const [ count, setCount ] = useState(0);
+
+//   return (
+//     <div>
+//       <h1>Count: {count}</h1>
+
+//       <button onClick={() => setCount(count + 1)}>
+//         Increase
+//       </button>
+
+//       <button onClick={() => setCount(count - 1)}>
+//         Decrease
+//       </button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// pratice problem no 1
+// import React from 'react'
+// import { useState } from "react";
+
+// const App = () => {
+//   const [count, setCount ] = useState(0);
+//   return (
+//     <div>
+//       <h1>Count: {count}</h1>
+
+//       <button onClick={() => setCount(count + 2)}>
+//         Increase
+//       </button>
+
+//       <button onClick={() => setCount(count - 2)}>
+//         Decrease
+//       </button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+// pratice problem no 2
+// import React from 'react'
+// import { useState } from 'react'
+
+// const App = () => {
+//   const [count, setCount] = useState("Abdullah");
+//   return (
+//     <div>
+//       <h1>Name: {count}</h1>
+
+//       <button onClick={() => setCount("Ali")}>
+//         Change Name
+//       </button>
+//     </div>
+//   )
+// }
+// export default App
+
+
+// pratice problem no 2
+// import React from 'react'
+// import { useState } from 'react'
+
+// const App = () => {
+//   const [count, setCount] = useState(0);
+//   return (
+//     <div>
+//       <h1>Count: {count}</h1>
+//      <button onClick={() => setCount(count + 1)}>
+//         Increase
+//      </button>
+//      <button onClick={() => setCount(count -1)}>
+//         Decrease
+//      </button>
+
+//     </div>
+//   )
+// }
+// export default App
+
+
+
+// pratice problem no 2
+// import React, { useState } from "react";
+
+// const App = () => {
+//   const [showPassword, setShowPassword] = useState(false);
+
+//   return (
+//     <div>
+//       <input
+//         type={showPassword ? "text" : "password"}
+//         placeholder="Enter password"
+//       />
+
+//       <button onClick={() => setShowPassword(!showPassword)}>
+//         {showPassword ? "Hide Password" : "Show Password"}
+//       </button>
+//     </div>
+//   );
+// };
+// export default App;
